234d4466aa108a0748a15a88d6632dec26098742
Fatima Qaisar <fatima79176985@gmail.com>
2026-01-10 00:31:59 +0500

    Add Task 2

---
+++ Lab-2/Task_2/src/task2/Student.java
@@ -0,0 +1,41 @@
+package task2;
+public class Student {
+    private String name;
+    private String email;
+    private double CGPA; 
+
+    public Student(String name, String email, double CGPA){
+        this.name=name;
+        this.email=email;
+        this.CGPA=CGPA;
+    }
+    public Student(){}
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public void setCGPA(double CGPA){
+        this.CGPA=CGPA;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public double getCGPA(){
+        return CGPA;
+    }
+    public void display(){
+        System.out.println("---Student Details---");
+        System.out.println("Name: "+ this.name);
+        System.out.println("Email: "+ this.email);
+        System.out.println("CGPA: "+ this.CGPA);
+    }
+}
