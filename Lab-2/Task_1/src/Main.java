ddfed2968bcba568661dbdfa7c4dc1f95bc40a77
Fatima Qaisar <fatima79176985@gmail.com>
2026-01-09 20:52:48 +0500

    Add Lab-2 Task 1

---
+++ Lab-2/Task_1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Student student =new Student();
+        student.setName("Fatima Qaisar");
+        student.setEmail("fatimaqaisar7917@gmail.com");
+        student.setCGPA(3.86);
+        student.display();
+    }
+}
---
+++ Lab-2/Task_1/src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    private String name;
+    private String email;
+    private double CGPA; 
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public void setCGPA(double CGPA){
+        this.CGPA=CGPA;
+    }
+    public void display(){
+        System.out.println("---Student Details---");
+        System.out.println("Name: "+ this.name);
+        System.out.println("Email: "+ this.email);
+        System.out.println("CGPA: "+ this.CGPA);
+    }
+}
